7b8d933a94bf90b17c00d202bad6c73cb346f57b
wanghui <1446218560@qq.com>
2020-06-08 13:23:30 +0800

    解决中文乱码的问题

--- src/main/java/com/example/demo/base/CharsetConfig.java
+++ src/main/java/com/example/demo/base/CharsetConfig.java
@@ -1,8 +1,17 @@
 package com.example.demo.base;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Locale;
 
 /**
 *@Description 中文乱码解决
@@ -11,7 +20,43 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 *@Time 14:09
 */
 @Configuration
-public class CharsetConfig implements WebMvcConfigurer {
+public class CharsetConfig extends WebMvcConfigurationSupport {
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        //localeResolver.setCookieName("lang");
+        //设置默认区域
+        localeResolver.setDefaultLocale(new Locale("en", "US"));
+        //设置cookie有效期.
+        localeResolver.setCookieMaxAge(-1);
+
+        return localeResolver;
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
+        super.addResourceHandlers(registry);
+    }
+
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
 
 }
